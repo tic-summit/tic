af9d108a59e9819cfb7ffced97bca1da42015f80
kins <clintonenu18@gmail.com>
2025-07-04 14:23:22 +0100

    hero page

--- app/test/Header2.jsx
+++ app/test/Header2.jsx
@@ -8,9 +8,10 @@ import Link from 'next/link'
 import React, { useEffect, useRef, useState } from 'react'
 
 function Header2() {
-        const [openMenu, setIsOpenMenu] = useState(false);
-            const searchRef = useRef(null);
-               const [isClicked, setIsClicked] = useState(false);
+    const [openMenu, setIsOpenMenu] = useState(false);
+    const searchRef = useRef(null);
+    const [isClicked, setIsClicked] = useState(false);
+    const [isActive, setIsActive] = useState('/')
 
     useEffect(() => {
 
@@ -24,9 +25,9 @@ function Header2() {
     })
 
 
-    
 
-        const Navbar = () => (
+
+    const Navbar = () => (
         <div>
             <ul className='text-[16px]  text-brand flex flex-col justify-center lg:flex-row items-center gap-8 font-semibold'>
                 <li><Link href={'/'}>Home</Link></li>
@@ -39,7 +40,7 @@ function Header2() {
     )
 
     const MobileMenu = () => (
-        <motion.div initial={{ y: "-100%", opacity: 0 }} exit={{ y: "-100%", opacity: 0 }} animate={{y: 0, opacity: 1 }} transition={{duration: 0.5}}  className='p-6 w-fit  fixed h-fit inset-0 bg-white'>
+        <motion.div initial={{ y: "-100%", opacity: 0 }} exit={{ y: "-100%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5 }} className='p-6 w-fit  fixed h-fit inset-0 bg-white'>
             <div className="flex justify-end text-gray-700" onClick={() => setIsOpenMenu(false)}><X className='h-6 w-6' /></div>
             <div className="content flex flex-col justify-between h-full  space-y-10">
                 <Navbar />
@@ -47,9 +48,9 @@ function Header2() {
             </div>
         </motion.div>
     )
-  return (
-    <div>
-       <div className="header h-24 px-4 sticky left-0 right-0">
+    return (
+        <div>
+            <div className="header h-24 px-4 sticky left-0 right-0">
                 <div className='max-w-[1400px] mx-auto h-full flex items-center justify-between'>
                     <div className="right-section">
                         <Logo />
@@ -75,7 +76,7 @@ function Header2() {
                                 </div>
                                 <div className="relative">
                                     <div variant="outline" className="border-none bg-transparent p-2">
-                                        <Bell  className="w-6 h-6" />
+                                        <Bell className="w-6 h-6" />
                                     </div>
                                     <div className="absolute -top-1 right-1 bg-green-600 text-white text-xs flex items-center justify-center w-5 h-5 rounded-full">
                                         0
@@ -90,14 +91,14 @@ function Header2() {
                                     <div onClick={() => setIsOpenMenu(true)}><Menu className='w-6 h-6' /></div>
                                     {/* Mobile Menu */}
                                     <div className="wrap">
-                                    <AnimatePresence>
-                                          {
-                                        openMenu &&(
-                                            
-                                                <MobileMenu />
-                                        )
-                                      }
-                                    </AnimatePresence>
+                                        <AnimatePresence>
+                                            {
+                                                openMenu && (
+
+                                                    <MobileMenu />
+                                                )
+                                            }
+                                        </AnimatePresence>
                                     </div>
                                 </div>
                             </div>
@@ -105,8 +106,8 @@ function Header2() {
                     </div>
                 </div>
             </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Header2
